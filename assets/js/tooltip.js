9bb88e9b92d422ee195995e45990c6930409d2bb
Daniel Dietrich <cafebab3@gmail.com>
2023-12-16 13:17:33 +0000

    Suppress tooltips on touch devices

--- assets/js/tooltip.js
+++ assets/js/tooltip.js
@@ -1,31 +1,36 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  const tooltip = document.getElementById('tooltip');
 
-  // Function to position and show the tooltip with text
-  const showTooltip = (element, message) => {
-    tooltip.textContent = message;
-    tooltip.style.display = 'block'; // Temporarily display to calculate dimensions
-    const elementRect = element.getBoundingClientRect();
-    tooltip.style.left = `${elementRect.left + (elementRect.width / 2) - (tooltip.offsetWidth / 2)}px`;
-    tooltip.style.top = `${elementRect.bottom + window.scrollY + 10}px`; // Position below with 10px gap
-    tooltip.classList.add('is-active');
-    tooltip.style.display = ''; // Reset display to use CSS for showing/hiding
-  };
+  const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints;
 
-  // Function to hide the tooltip
-  const hideTooltip = () => {
-    tooltip.removeAttribute('style');
-    tooltip.classList.remove('is-active');
-    tooltip.textContent = '';
-  };
+  if (!isTouchDevice) {
+    const tooltip = document.getElementById('tooltip');
 
-  // Attach event listeners to elements with a 'data-tooltip' attribute
-  document.querySelectorAll('[data-tooltip]').forEach((element) => {
-    element.addEventListener('mouseenter', (event) => {
-        const message = event.target.getAttribute('data-tooltip');
-        showTooltip(element, message);
+    // Function to position and show the tooltip with text
+    const showTooltip = (element, message) => {
+      tooltip.textContent = message;
+      tooltip.style.display = 'block'; // Temporarily display to calculate dimensions
+      const elementRect = element.getBoundingClientRect();
+      tooltip.style.left = `${elementRect.left + (elementRect.width / 2) - (tooltip.offsetWidth / 2)}px`;
+      tooltip.style.top = `${elementRect.bottom + window.scrollY + 10}px`; // Position below with 10px gap
+      tooltip.classList.add('is-active');
+      tooltip.style.display = ''; // Reset display to use CSS for showing/hiding
+    };
+
+    // Function to hide the tooltip
+    const hideTooltip = () => {
+      tooltip.removeAttribute('style');
+      tooltip.classList.remove('is-active');
+      tooltip.textContent = '';
+    };
+
+    // Attach event listeners to elements with a 'data-tooltip' attribute
+    document.querySelectorAll('[data-tooltip]').forEach((element) => {
+      element.addEventListener('mouseenter', (event) => {
+          const message = event.target.getAttribute('data-tooltip');
+          showTooltip(element, message);
+      });
+      element.addEventListener('mouseleave', hideTooltip);
+      element.addEventListener('click', hideTooltip);
     });
-    element.addEventListener('mouseleave', hideTooltip);
-    element.addEventListener('click', hideTooltip);
-  });
+  }
 });
